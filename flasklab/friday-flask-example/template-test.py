7e7807ae8eb6cf9b88a16b5f081aea3d56dc51be
noazlee <noahzlee5@gmail.com>
2024-03-25 12:52:16 -0500

    added flask warmup

---
+++ flasklab/friday-flask-example/template-test.py
@@ -0,0 +1,69 @@
+from sqlite3 import OperationalError
+from flask import Flask
+from flask import render_template
+import random
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return render_template("index.html")
+
+@app.route('/<operation>/<x>/<y>')
+def rand(x, y, operation):
+    #Input values that come from a URL (i.e., @app.route)
+    #   are always strings so I need to convert the type to int
+    x = int(x)
+    y = int(y)
+    inputs = [x,y]
+    question = operation 
+    answer = None
+
+    if question == "plus":
+        answer = x + y
+    elif question == "minus":
+        answer = x - y
+    elif question == "times":
+        answer = x * y
+    elif question == "divide":
+        answer = round((x/y),2)
+    else:
+        answer = None
+    
+    json_answer = {
+        "Question":question,
+        "Inputs":inputs,
+        "Answer":answer 
+    }
+
+    return json.dumps(json_answer)
+
+
+    
+    
+    
+
+
+
+@app.route('/randS')
+def randSentence():
+    listNames = ["Jeremiah", "Noah", "Warren", "Daya"]
+    listAdj = ["Coward", "Loser", "Poopy", "Ugly", "Musty", "Crusty"]
+    listPlace = ["O-Block", "Italy", "Chicago", "Ugly-stan"]
+    listYear = ["5 BC", "2005", "2004", "2000", "1945", "1998"]
+    num1 = random.randint(0,len(listNames)-1)
+    name = listNames[num1]
+    num2 = random.randint(0,len(listAdj)-1)
+    adj = listAdj[num2] 
+    num3 = random.randint(0,len(listPlace)-1)
+    place = listPlace[num3]
+    num4 = random.randint(0,len(listYear)-1)
+    year = listYear[num4]
+
+    return render_template("randStory.html", randName=name, randAdjective=adj,randPlace=place,randYear=year)
+
+
+if __name__ == '__main__':
+    my_port = 5128
+    app.run(host='0.0.0.0', port = my_port) 
