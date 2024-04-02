f166b405836ea52620760f2d10a151435f8579b8
noazlee <noahzlee5@gmail.com>
2024-04-02 18:17:05 -0500

    added docker test:

--- todo-application/NOTES.md
+++ todo-application/NOTES.md
@@ -2,14 +2,14 @@
 
 go mod init github.com/noazlee/todo-application
 
-## Install Fiber v2
+## Install Fiber v2 - in server
 
 go get -u github.com/gofiber/fiber/v2
 
-## Create client app with Vite - Bootstrap application
+## Create client app with Vite - Bootstrap application - in root
 
 yarn create vite client -- --template react-ts
 
-## Install dependencies
+## Install dependencies - in client
 
 yarn add @mantine/hooks @mantine/core swr @primer/octicons-react
--- todo-application/server/main.go
+++ todo-application/server/main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
 
 func main() {
 	fmt.Print("Hello World")
+
+	app := fiber.New() // : allows us to infer the type for app
+
+	todos := []Todo{}
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK") // means our app is up and running
+	})
+
+	// add a todo
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+
+	})
+
+	// mark a todo as done
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+
+	})
+
+	// fetch all todos
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
+
+	// go run main.go -- to run
 }
